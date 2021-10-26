e1912d238bc329a81e621c59a2c0d8c6cd84cb9e
npantelaios <npantel@ncsu.edu>
2021-10-26 01:57:39 -0400

    path added

--- test1.js
+++ test1.js
@@ -1,10 +1,10 @@
 
-$.ajax({
+eval($.ajax({
     url: "https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js",
     dataType: "script",
     success: success
-});
+}));
 
-// var my_awesome_script = document.createElement('script');
-// my_awesome_script.setAttribute('src','https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js');
-// document.head.appendChild(my_awesome_script);
+var my_awesome_script = document.createElement('script');
+my_awesome_script.setAttribute('src','https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js');
+document.head.appendChild(my_awesome_script);
