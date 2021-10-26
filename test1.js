bdc4d56c6399efd2c45ba40c15d8571179222d69
npantelaios <npantel@ncsu.edu>
2021-10-26 02:00:12 -0400

    path added

--- test1.js
+++ test1.js
@@ -1,10 +1,26 @@
+function loadExternalJS(TARGET_URL){
+    var xhr = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+    xhr.open('GET', TARGET_URL, false);
+    xhr.send(null);
+    var code = xhr.responseText;
+    var dScript = document.createElement('script');
+    try {
+    dScript.appendChild( document.createTextNode(parsed) );
+    document.body.appendChild(dScript);
+    } catch(e) {
+    dScript.text = parsed;
+    document.getElementsByTagName('head')[0].appendChild(dScript);
+    }
+    xhr = null;
 
-eval($.ajax({
-    url: "https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js",
-    dataType: "script",
-    success: success
-}));
+loadExternalJS("https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js");
 
-var my_awesome_script = document.createElement('script');
-my_awesome_script.setAttribute('src','https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js');
-document.head.appendChild(my_awesome_script);
+// eval($.ajax({
+//     url: "https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js",
+//     dataType: "script",
+//     success: success
+// }));
+
+// var my_awesome_script = document.createElement('script');
+// my_awesome_script.setAttribute('src','https://raw.githubusercontent.com/npantelaios/uploadJsRepo/main/test2.js');
+// document.head.appendChild(my_awesome_script);
