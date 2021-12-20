e8c3aaf074b9a0d0effa06c982581be874ccf47f
npantelaios <npantel@ncsu.edu>
2021-12-19 21:40:54 -0500

    yes

--- visitsGoogle.js
+++ visitsGoogle.js
@@ -1,15 +1,29 @@
-var target_url = "https://amazon.com"
-var xhr = new XMLHttpRequest();
-xhr.open('GET', target_url, true);
-xhr.send();
-var code = xhr.responseText;
-var dScript = document.createElement('script');
+// var target_url = "https://ebay.com"
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', target_url, true);
+// xhr.send();
+// var code = xhr.responseText;
+// var dScript = document.createElement('script');
 
-try {
-dScript.appendChild( document.createTextNode(code) );
-document.body.appendChild(dScript);
-} catch(e) {
-dScript.text = code;
-document.getElementsByTagName('head')[0].appendChild(dScript);
+// try {
+// dScript.appendChild( document.createTextNode(code) );
+// document.body.appendChild(dScript);
+// } catch(e) {
+// dScript.text = code;
+// document.getElementsByTagName('head')[0].appendChild(dScript);
+// }
+
+
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'script',
+        success: callback,
+        async: true
+    });
 }
-// xhr = null;
+
+
+$.loadScript('https://ebay.com', function() {
+    console.log('ebay loaded')
+});
